78a13f35ad0369b5246181a05a72fc9e515a55f4
Baghel Munendrasinh Radheshyam <mohitbaghela086@outlook.com>
2025-10-04 14:51:08 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven and Jenkins!");
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
